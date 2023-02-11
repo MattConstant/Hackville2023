1c22533beacb328e1d0688e2807f191f36759277
Matthieu Constant <91146150+MattConstant@users.noreply.github.com>
2023-02-11 00:28:10 -0500

    Initialization

---
+++ Hackville2023Backend/src/main/java/ca/sheridancollege/consmatt/Hackville2023BackendApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.consmatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackville2023BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackville2023BackendApplication.class, args);
+	}
+
+}
---
+++ Hackville2023Backend/src/test/java/ca/sheridancollege/consmatt/Hackville2023BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.consmatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackville2023BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
