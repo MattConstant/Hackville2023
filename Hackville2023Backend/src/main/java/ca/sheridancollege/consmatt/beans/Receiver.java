3f8d96d04dba61a19ad4e07898912fe704c0a4b5
MattConstant <91146150+MattConstant@users.noreply.github.com>
2023-02-11 15:08:41 -0500

    added requestor and donator beans

---
+++ Hackville2023Backend/src/main/java/ca/sheridancollege/consmatt/beans/Receiver.java
@@ -0,0 +1,23 @@
+package ca.sheridancollege.consmatt.beans;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Document
+@Builder
+public class Receiver {
+	@Id
+	private String id;
+	private String phoneRequested;
+	private String name;
+	private String phoneNumber;
+	private String address;
+}
--- Hackville2023Backend/src/main/java/ca/sheridancollege/consmatt/bootstrap/BootstrapData.java
+++ Hackville2023Backend/src/main/java/ca/sheridancollege/consmatt/bootstrap/BootstrapData.java
@@ -3,29 +3,39 @@ package ca.sheridancollege.consmatt.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import ca.sheridancollege.consmatt.PhoneRepository;
-import ca.sheridancollege.consmatt.beans.Phone;
+import ca.sheridancollege.consmatt.beans.Donator;
+import ca.sheridancollege.consmatt.beans.Receiver;
+import ca.sheridancollege.consmatt.repositories.DonatorRepository;
+import ca.sheridancollege.consmatt.repositories.ReceiveRepository;
 import lombok.AllArgsConstructor;
 
 @Component
 @AllArgsConstructor
 public class BootstrapData implements CommandLineRunner {
     
-    private PhoneRepository pRepo;
+    private DonatorRepository dRepo;
+    private ReceiveRepository rRepo;
+
     
     @Override
     public void run(String... args) throws Exception {        
         
-        pRepo.deleteAll();
+        dRepo.deleteAll();
+        rRepo.deleteAll();
         
-        Phone p1 = Phone.builder().title("iphone 6").build();
-        Phone p2 = Phone.builder().title("iphone 7").build();
-        Phone p3 = Phone.builder().title("iphone 4s").build();
+        Donator d1 = Donator.builder().title("iphone 6").category("Phones").description("Brand new").damage("No damage").address("24 Manoir drive, Ottawa, Ontario, K0A 1W0").name("Jim Buhler").phoneNumber("6134431216").email("jimmyjim@gmail.com").build();
+        Donator d2 = Donator.builder().title("iphone 6").category("Phones").description("Brand new").damage("No damage").address("24 Manoir drive, Ottawa, Ontario, K0A 1W0").name("Jim Buhler").phoneNumber("6134431216").email("jimmyjim@gmail.com").build();
+        Donator d3 = Donator.builder().title("iphone 6").category("Phones").description("Brand new").damage("No damage").address("24 Manoir drive, Ottawa, Ontario, K0A 1W0").name("Jim Buhler").phoneNumber("6134431216").email("jimmyjim@gmail.com").build();
 
         
-        pRepo.save(p1);
-        pRepo.save(p2);
-        pRepo.save(p3);
+        dRepo.save(d1);
+        dRepo.save(d2);
+        dRepo.save(d3);
+        
+        Receiver r1 = Receiver.builder().phoneRequested("iphone 6").name("Hairy Henry").phoneNumber("1234567891").address("16 kimber drive, Ottawa, Ontario, K0A 1W0").build();
+        
+        rRepo.save(r1);
+
         
 
         
